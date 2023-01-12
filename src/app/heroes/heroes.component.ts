37edef6d38b2a0a8f6cead16faea7aef0636c8eb
Loc Diep Huu <dhloc1@tma.com.vn>
2023-01-12 13:54:20 +0700

    updated project

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,42 @@
+import { MessageService } from './../message.service';
+import { HeroService } from './../hero.service';
+import { Hero } from './../hero';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService.addHero({ name } as Hero).subscribe((hero) => {
+      this.heroes.push(hero);
+    });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter((h) => h.id !== hero.id);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
+}
